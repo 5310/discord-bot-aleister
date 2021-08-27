4d2f0ca3cc651729f666bc29786766a683b2238f
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-08-27 22:16:23 +0530

    Housekeeping

--- api/interactions.ts
+++ api/interactions.ts
@@ -5,10 +5,18 @@ import { readAll } from "https://deno.land/std@0.106.0/io/util.ts";
 const PUBLIC_KEY = <string> Deno.env.get("DISCORD_PUBLIC_KEY");
 
 export default async (req: ServerRequest) => {
+  // Human-readable message for strangers
+  if (req.method === "GET") {
+    req.respond({
+      body: "This is a POST-only endpoint",
+    });
+    return;
+  }
+
+  // Verify and validate Discord's request
   const signature = req.headers.get("X-Signature-Ed25519") ?? "";
   const timestamp = req.headers.get("X-Signature-Timestamp") ?? "";
   const body = new TextDecoder().decode(await readAll(req.body));
-
   const isReqValid = req.method === "POST" && !!signature && !!timestamp;
   if (!isReqValid) {
     req.respond({
@@ -16,7 +24,6 @@ export default async (req: ServerRequest) => {
     });
     return;
   }
-
   const isSigValid = nacl.sign.detached.verify(
     new TextEncoder().encode(timestamp + body),
     hexToUint8Array(signature),
@@ -29,124 +36,45 @@ export default async (req: ServerRequest) => {
     return;
   }
 
+  // Extract the Interaction
   const interaction = JSON.parse(body);
   console.debug(interaction);
 
+  // Ping
   if (interaction.type === 1) {
-    req.respond({
-      status: 200,
-      headers: new Headers({
-        "content-type": "application/json; charset=utf8",
-      }),
-      body: JSON.stringify({ type: 1 }),
-    });
+    req.respond(jsonResponse({
+      type: 1,
+    }));
     return;
   }
 
+  // Chat input
   if (interaction.type === 2) {
-    req.respond({
-      status: 200,
-      headers: new Headers({
-        "content-type": "application/json; charset=utf8",
-      }),
-      body: JSON.stringify({
-        type: 4,
-        data: {
-          content: `Hello, ${interaction.data.options[0].value}!`,
-        },
-      }),
-    });
+    req.respond(jsonResponse({
+      type: 4,
+      data: {
+        content: `Hello, ${interaction.data.options[0].value}!`,
+      },
+    }));
     return;
   }
 
+  // Catch-all
   req.respond({
     status: 400,
   });
 };
 
-// The main logic of the Discord Slash Command is defined in this function.
-// async function interactionsHandler(request: Request) {
-//   // validateRequest() ensures that a request is of POST method and
-//   // has the following headers.
-//   const { error } = await validateRequest(request, {
-//     POST: {
-//       headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
-//     },
-//   });
-//   if (error) {
-//     return json({ error: error.message }, { status: error.status });
-//   }
+function jsonResponse(body: unknown) {
+  return {
+    status: 200,
+    headers: new Headers({
+      "content-type": "application/json; charset=utf8",
+    }),
+    body: JSON.stringify(body),
+  };
+}
 
-//   // verifySignature() verifies if the request is coming from Discord.
-//   // When the request's signature is not valid, we return a 401 and this is
-//   // important as Discord sends invalid requests to test our verification.
-//   const { valid, body } = await verifySignature(request);
-//   if (!valid) {
-//     return json(
-//       { error: "Invalid request" },
-//       {
-//         status: 401,
-//       },
-//     );
-//   }
-
-//   const interaction: { type: number; data: { name: string; options: [] } } =
-//     JSON.parse(body);
-//   const { type, data } = interaction;
-//   // Discord performs Ping interactions to test our application.
-//   // Type 1 in a request implies a Ping interaction.
-//   if (type === 1) {
-//     return json({
-//       type: 1, // Type 1 in a response is a Pong interaction response type.
-//     });
-//   }
-
-//   // Type 2 in a request is an ApplicationCommand interaction.
-//   // It implies that a user has issued a command.
-//   if (type === 2) {
-//     return json({
-//       type: 4,
-//       data: {
-//         content: JSON.stringify(interaction),
-//       },
-//     });
-//     //   const { value } = data.options.find((option: { name: string }) =>
-//     //     option.name === "name"
-//     //   );
-//     //   return json({
-//     //     // Type 4 responds with the below message retaining the user's
-//     //     // input at the top.
-//     //     type: 4,
-//     //     data: {
-//     //       content: `Hello, ${value}!`,
-//     //     },
-//     //   });
-//   }
-
-//   // We will return a bad request error as a valid Discord request
-//   // shouldn't reach here.
-//   return json({ error: "bad request" }, { status: 400 });
-// }
-
-// /** Verify whether the request is coming from Discord. */
-// async function verifySignature(
-//   request: Request,
-// ): Promise<{ valid: boolean; body: string }> {
-//   const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY")!;
-//   // Discord sends these headers with every request.
-//   const signature = request.headers.get("X-Signature-Ed25519")!;
-//   const timestamp = request.headers.get("X-Signature-Timestamp")!;
-//   const body = await request.text();
-//   const valid = nacl.sign.detached.verify(
-//     new TextEncoder().encode(timestamp + body),
-//     hexToUint8Array(signature),
-//     hexToUint8Array(PUBLIC_KEY),
-//   );
-
-//   return { valid, body };
-// }
-
-/** Converts a hexadecimal string to Uint8Array. */
 function hexToUint8Array(hex: string) {
   return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
 }
